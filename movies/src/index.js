2f1f91f5e2c338c4495ddf2a93b18d7f83a1dc18
mrn-dns <marincasdenis@yahoo.com>
2023-10-07 17:58:12 +0300

    Movie Details Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,3 +1,4 @@
+import MovieDetailsPage from './pages/movieDetailsPage'
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
@@ -83,12 +84,25 @@ const sample = {
   vote_count: 9692,
 };
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
+
 const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+// const App = () => {
+//   return (
+//       <HomePage movies={movies} />
+//   );
+// };
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
 
 const rootElement = createRoot( document.getElementById("root") )
