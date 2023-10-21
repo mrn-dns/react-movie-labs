1fe56f2ad680d12ba60fc5562039a255efbd233e
mrn-dns <marincasdenis@yahoo.com>
2023-10-21 18:52:01 +0300

    Fix backward/forward arrows.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
